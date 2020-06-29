4179155686176954b45aecdd76f3b110e667bfdd
Ankit Kumar <AnkitKumar@Ankits-Macbook-pro.local>
2020-06-29 22:19:56 +0530

    Updates

---
+++ precache-manifest.be4e098862fbc46e6859021c68305906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d3f6a6f56988e0f4b3e993c016d369",
+    "url": "/redux-basic/index.html"
+  },
+  {
+    "revision": "312a3d2a2ae5625b0cc9",
+    "url": "/redux-basic/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "312a3d2a2ae5625b0cc9",
+    "url": "/redux-basic/static/js/2.95731a91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-basic/static/js/2.95731a91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363c6b5daccd608dfd49",
+    "url": "/redux-basic/static/js/main.25383fe9.chunk.js"
+  },
+  {
+    "revision": "b3ca8b1a2c495d014e7b",
+    "url": "/redux-basic/static/js/runtime-main.f85c9deb.js"
+  }
+]);
